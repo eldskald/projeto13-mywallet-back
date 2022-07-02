b3a1dd297e633d309d1a10ba22c03b4c527be292
rafael <rafaelbordoni00@gmail.com>
2022-07-02 00:49:01 -0300

    Prepare for deploy

--- src/db.js
+++ src/db.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const MONGO_URI = process.env.MONGO_URI;
+const DB_NAME = process.env.DB_NAME;
 
 const dbClient = new MongoClient(MONGO_URI);
 export let db;
@@ -10,7 +11,7 @@ export let db;
 export async function connectDatabase () {
     try {
         await dbClient.connect();
-        const database = dbClient.db('my-wallet');
+        const database = dbClient.db(DB_NAME);
         db = {
             accounts: database.collection('accounts'),
             sessions: database.collection('sessions'),
